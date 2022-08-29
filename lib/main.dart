cb228f5370a6eeab39b191d5c97d44e964f1981c
838 <838@mail.com>
2022-08-29 20:28:22 +0200

    ios VO three finger scroll

--- lib/main.dart
+++ lib/main.dart
@@ -11,24 +11,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: CustomScrollView(
-          slivers: <Widget>[
-            const SliverAppBar(
-              title: Text("Spoken Content Example"),
-              floating: true,
-              pinned: true,
-            ),
-            SliverList(
-              delegate: SliverChildListDelegate(
-                List.generate(
-                  15,
-                  (index) => Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      'Sliver Item ${index.toString()}',
-                      style: const TextStyle(fontSize: 22.0),
-                    ),
-                  ),
+        body: ListView(
+          shrinkWrap: true,
+          children: [
+            ...List.generate(
+              15,
+              (index) => Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: Text(
+                  'Sliver Item ${index.toString()}',
+                  style: const TextStyle(fontSize: 22.0),
                 ),
               ),
             ),

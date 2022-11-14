f9505a7e7a82a7b1f91fcfe82f4a0ecea280eb30
838 <838@mail.com>
2022-11-14 16:47:31 +0100

    Revert "Android accesibility button press"
    This reverts commit 64a40b8f966a423d7bbe0f7ea88fdb29c568a796.

--- android/app/src/main/kotlin/com/example/fluttemo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/fluttemo/MainActivity.kt
@@ -1,13 +1,6 @@
 package com.example.fluttemo
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity : FlutterActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        flutterEngine
-                .platformViewsController
-                .registry
-                .registerViewFactory("<platform-view-type>", NativeViewFactory())
-    }
+class MainActivity: FlutterActivity() {
 }

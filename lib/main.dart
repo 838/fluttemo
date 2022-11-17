7f156bc3fd0dcce3d4fe6f7841457d633a8fedb6
838 <838@mail.com>
2022-11-17 15:39:25 +0100

    new demo for ios 16 semantic focus

--- lib/main.dart
+++ lib/main.dart
@@ -7,33 +7,80 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: CustomScrollView(
-          slivers: <Widget>[
-            const SliverAppBar(
-              title: Text("SliverList Example"),
-              expandedHeight: 100.0,
-              floating: true,
-              pinned: true,
-            ),
-            SliverList(
-              delegate: SliverChildListDelegate(
-                List.generate(
-                  100,
-                  (index) => Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      'Sliver Item ${index.toString()}',
-                      style: const TextStyle(fontSize: 22.0),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Semantics(
+          label: 'Open `Track name` in full screen player',
+          button: true,
+          onTap: () {},
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Semantics(
+                    label: 'Close',
+                    button: true,
+                    excludeSemantics: true,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.close),
                     ),
                   ),
-                ),
+                  const ExcludeSemantics(
+                    child: Text('Track name'),
+                  ),
+                  Semantics(
+                    label: 'Play',
+                    button: true,
+                    excludeSemantics: true,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.play_arrow),
+                    ),
+                  ),
+                  Semantics(
+                    label: 'Pause',
+                    button: true,
+                    excludeSemantics: true,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.pause),
+                    ),
+                  )
+                ],
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );

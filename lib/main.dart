2f850faa8546efc72f4cdefdbbe3e0707d8e1f8e
838 <838@mail.com>
2023-03-29 11:43:37 +0200

    Revert "Listview indexed semantics"
    This reverts commit 04da0121632f455b35e725024bdbc6f4ef060a8e.

--- lib/main.dart
+++ lib/main.dart
@@ -30,22 +30,38 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: SizedBox(
-        child: ListView(
-          addSemanticIndexes: false,
-          semanticChildCount: 3,
-          children: const <Widget>[
-            IndexedSemantics(index: 0, child: Text('First')),
-            IndexedSemantics(index: 1, child: Text('Second')),
-            IndexedSemantics(index: 2, child: Text('Third')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
